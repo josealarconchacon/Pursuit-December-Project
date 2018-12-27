e3ebf30203e20f47c6c284e75d5d1afde166daf8
josealarconchacon <jose.alarcon.chacom@gmail.com>
2018-12-27 15:58:29 -0600

    commit change

--- MoviesFavorite/Controllers/DetailViewController.swift
+++ MoviesFavorite/Controllers/DetailViewController.swift
@@ -11,15 +11,27 @@ import UIKit
 class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var detailViewText: UITextView!
+    @IBOutlet weak var labelDate: UILabel!
+    
+    @IBOutlet weak var favorites: UIButton!
+    @IBOutlet weak var noFavorites: UIButton!
+    
     
     var selectedMovie: Movie?
+    var date: Movie!
     
     let imageBaseUrlString = "https://image.tmdb.org/t/p/w500"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         movieSetUp()
+        detailViewText.text = selectedMovie?.overview
+        labelDate.text = selectedMovie?.release_date
+        labelDate.text = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .none)
     }
+    
     private func movieSetUp() {
         let backdropUrlString = imageBaseUrlString + (selectedMovie?.poster_path)!
         if let backdropURL = URL(string: backdropUrlString) {
--- MoviesFavorite/Controllers/TableViewController.swift
+++ MoviesFavorite/Controllers/TableViewController.swift
@@ -9,48 +9,50 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    
    // @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     
-    let movieAPI = URL.init(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=&language=en-US&page=1")
+    let movieAPI = URL.init(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a89086b4927405c65e442226c571beb6&language=en-US&page=1")
     let imagesBaseUrlString = "https://image.tmdb.org/t/p/w500"
     
-    
     var results = [Movie]()
-    //var movieToFilter = [Movie]()
-    //var sortWhenSearching = [Movie]()
+    //1 (Array of movies filtered by search term)
+    var filteredMovies = [Movie]()
+    var sortWhenNotSearching = [Movie]()
+    var sortWhenSearching = [Movie]()
     
     // setting the search bar
-    //let searchController = UISearchController(searchResultsController: nil)
+    let searchController = UISearchController(searchResultsController: nil)
     
-//    func searchIsEmpty() -> Bool {
-//        return searchController.searchBar.text?.isEmpty ?? true
-//    }
-//    func filterContentForSearchText(_ searchText: String, scope: String = "ALL") {
-//        movieToFilter = results.filter({( user : Movie) -> Bool in
-//            let value = user.title.contains(searchText.lowercased())
-//        return value
-//        })
-//        tableView.reloadData()
-//    }
-//    func filter() -> Bool {
-//        return !searchIsEmpty()
-//    }
+    func searchIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredMovies = results.filter({( movie : Movie) -> Bool in
+            let value = movie.title.lowercased().contains(searchText.lowercased())
+        return value
+        })
+        tableView.reloadData()
+    }
+    func filter() -> Bool {
+        return !searchIsEmpty()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("the view loaded")
-//        definesPresentationContext = true
-//        searchController.searchResultsUpdater = self
-//        searchController.searchBar.placeholder = "earch for a movie"
-//        searchController.obscuresBackgroundDuringPresentation = false
-//        navigationItem.searchController = searchController
+        // 4
+        definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for a movie"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+
+          tableView.dataSource = self
+          self.tableView.reloadData()
         
-//        tableView.dataSource = self
-//        self.tableView.reloadData()
-        
-        let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=&language=en-US&page=1"
+        let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=a89086b4927405c65e442226c571beb6&language=en-US&page=1"
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 print("attempting to parse json")
@@ -58,18 +60,14 @@ class TableViewController: UITableViewController {
             }
         }
     }
-    
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let indexPath = tableView.indexPathForSelectedRow,
-//        let description = segue.destination as? DetailViewController else { return}
-//        let setUpResult = results[indexPath.row]
         super.prepare(for: segue, sender: sender)
         let detailViewController = segue.destination as? DetailViewController
-        let selectedCell = sender as? MovieTableViewCell
         let indexPath = tableView.indexPathForSelectedRow
         let selectedMovie = results[indexPath!.row]
-        detailViewController?.selectedMovie = selectedMovie
-        
+        let movie = filter() ? filteredMovies[indexPath!.row]: results[indexPath!.row]
+        detailViewController?.selectedMovie = movie
     }
     
     func parseJson(json: Data) {
@@ -81,30 +79,39 @@ class TableViewController: UITableViewController {
         print("parsing movies data!")
        }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    if filter() {
-//        return movieToFilter.count
-//    }
+        // 2
+        if filter() {
+            print(filteredMovies)
+            return filteredMovies.count
+        }
         return results.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //sortWhenSearching  = movieToFilter.sorted(by: { $0.title < $1.title})
     
-//    var movieToSet = sortWhenSearching[indexPath.row]
-//    if filter() {
-//        movieToSet = movieToFilter[indexPath.row]
-//    } else {
-//        movieToSet = results[indexPath.row]
-//    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        sortWhenNotSearching = results.sorted(by: { $0.title < $1.title})
+        //3
+        sortWhenSearching  = filteredMovies.sorted(by: { $0.title < $1.title})
+    
+//        var movieToSet = sortWhenSearching[indexPath.row]
+        var movieToSet: Movie
+        
+        if filter() {
+            movieToSet = filteredMovies[indexPath.row]
+        } else {
+            movieToSet = results[indexPath.row]
+        }
     
         let cellIdentifier = "movieCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MovieTableViewCell
         let resulToSet = results[indexPath.row]
-        cell?.movieNameLabel.text = resulToSet.title
-        
+//        cell?.movieNameLabel.text = resulToSet.title
+        cell?.movieNameLabel.text = movieToSet.title
     
-        let backdropUrlString = imagesBaseUrlString + resulToSet.backdrop_path
+//        let backdropUrlString = imagesBaseUrlString + resulToSet.backdrop_path
+        let backdropUrlString = imagesBaseUrlString + movieToSet.backdrop_path
         if let bsckdropURL = URL(string: backdropUrlString) {
             DispatchQueue.global().async {
                 if let data = try? Data(contentsOf: bsckdropURL) {
@@ -121,41 +128,18 @@ class TableViewController: UITableViewController {
         return 1
     }
 }
-//    extension CGImage {
-//        var imageIsDark: Bool {
-//            get {
-//                guard let imageData = self.dataProvider?.data else { return false}
-//                guard let pt = CFDataGetBytePtr(imageData)  else { return false}
-//                let theLength = CFDataGetLength(imageData)
-//                let toSet = Int(Double(self.width * self.height) * 0.45)
-//                var darkPixels = 0
-//                for i in stride(from: 0, to: theLength, by: 4) {
-//                    let r = pt[i]
-//                    let g = pt[i + 1]
-//                    let b = pt[i + 2]
-//                    let luminance = (0.299 * Double(r) + 0.587 * Double(g) + 0.114 * Double(b))
-//                    if luminance < 150 {
-//                        darkPixels += 1
-//                        if darkPixels > toSet {
-//                            return true
-//                        }
-//                    }
-//                }
-//                return false
-//            }
-//        }
-//}
-//
-//
-//extension UIImage {
-//    var isDark: Bool {
-//        get {
-//            return self.cgImage?.isMask ?? false
+//extension TableViewController: UISearchBarDelegate {
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        searchBar.resignFirstResponder()
+//        guard let seachText = searchBar.text,
+//        !seachText.isEmpty,
+//            let searchTextEncode = seachText.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+//                return
 //        }
 //    }
 //}
-
-
-
-
-
+extension TableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
